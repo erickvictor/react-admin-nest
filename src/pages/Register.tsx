e1a9a4e0f2eb57a8b6860467c267364f5af800fb
Erick Bernardo <erickvictor@gmail.com>
2021-06-14 16:11:44 -0400

    insert form in component register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,74 @@
-import { Component } from "react";
+import { Component, SyntheticEvent } from "react";
+import "../Login.css";
 
 export default class Register extends Component {
+  first_name = "";
+  last_name = "";
+  email = "";
+  password = "";
+  password_confirm = "";
+
+  submit = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    console.log({
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email,
+      password: this.password,
+      password_confirm: this.password_confirm
+    });    
+  }
+
   render() {
     return (
-      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 className="h2">Register</h1>
-      </div>
+      <main className="form-signin">
+        <form onSubmit={this.submit}>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+          <input
+            className="form-control"
+            placeholder="First Name"
+            required
+            onChange={(e) => (this.first_name = e.target.value)}
+          />
+
+          <input
+            className="form-control"
+            placeholder="Last Name"
+            required
+            onChange={(e) => (this.last_name = e.target.value)}
+          />
+
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            required
+            onChange={(e) => (this.email = e.target.value)}
+          />
+
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            required
+            onChange={(e) => (this.password = e.target.value)}
+          />
+
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password Confirm"
+            required
+            onChange={(e) => (this.password_confirm = e.target.value)}
+          />
+
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Submit
+          </button>
+        </form>
+      </main>
     );
   }
 }
