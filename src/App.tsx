e3e21729470adc104e2549cb5d934242fc317290
Erick Bernardo <erickvictor@gmail.com>
2021-06-16 10:56:37 -0400

    create button edit user

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import UserCreate from "./pages/users/UserCreate";
+import UserEdit from "./pages/users/UserEdit";
 import Users from "./pages/users/Users";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path={"/login"} component={Login} />
         <Route path={"/users"} exact component={Users} />
         <Route path={"/users/create"} component={UserCreate} />
+        <Route path={"/users/:id/edit"} component={UserEdit} />
         
       </BrowserRouter>
     </div>
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -30,9 +30,8 @@ const Users = () => {
     }
   };
 
-
   const del = async (id: number) => {
-    if (window.confirm('Are you sure you want to delete this record?')) {
+    if (window.confirm("Are you sure you want to delete this record?")) {
       await axios.delete(`users/${id}`);
 
       setUsers(users.filter((u: User) => u.id !== id));
@@ -41,12 +40,14 @@ const Users = () => {
 
   return (
     <Wrapper>
-      <div className="table-responsive">  
+      <div className="table-responsive">
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 className="h2">Users</h1>
         </div>
         <div className="pt-3 pb-2 mb-3 border-bottom">
-          <Link to="/users/create" className="btn btn-sm btn-outline-secondary">Add</Link>
+          <Link to="/users/create" className="btn btn-sm btn-outline-secondary">
+            Add
+          </Link>
         </div>
         <table className="table table-striped table-sm">
           <thead>
@@ -70,6 +71,12 @@ const Users = () => {
                   <td>{user.role.name}</td>
                   <td>
                     <div className="btn-group mr-2">
+                      <Link
+                        className="btn btn-sm btn-outline-secondary"
+                        to={`/users/${user.id}/edit`}
+                      >
+                        Edit
+                      </Link>
                       <button
                         className="btn btn-sm btn-outline-secondary"
                         onClick={() => del(user.id)}
