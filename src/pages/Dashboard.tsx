e33ccb2698b8124f56d4f1d1dc99279708d56f81
Erick Bernardo <erickvictor@gmail.com>
2021-06-17 10:21:16 -0400

    insert chart in dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,51 @@
-import { Component } from "react";
 import Wrapper from "../components/Wrapper";
+import * as c3 from "c3";
+import { useEffect } from "react";
+import axios from "axios";
 
-export default class Dashboard extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 className="h2">Dashboard</h1>
-        </div>
-      </Wrapper>
-    );
-  }
-}
+const Dashboard = () => {
+  useEffect(() => {
+    (async () => {
+      const chart = c3.generate({
+        bindto: "#chart",
+        data: {
+          x: "x",
+          columns: [["x"], ["Sales"]],
+          types: {
+            Sales: "bar",
+          },
+        },
+        axis: {
+          x: {
+            type: "timeseries",
+            tick: {
+              format: "%Y-%m-%d",
+            },
+          },
+        },
+      });
+      const { data } = await axios.get('chart');
+
+      chart.load({
+        columns: [
+          ['x', ...data.map((r: any) => r.date)],
+          ['Sales', ...data.map((r: any) => r.sum)]
+        ]
+      })
+    })();
+  });
+
+  return (
+    <Wrapper>
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 className="h2">Dashboard</h1>
+      </div>
+      <div>
+        <h3 className="h3">Daily Sales</h3>
+        <div id="chart" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Dashboard;
